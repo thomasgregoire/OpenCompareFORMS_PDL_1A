8c4737d2eb0cc3ffc7664f88a15f7745cadef69b
Thomas Gregoire <thomas0035@gmail.com>
2016-11-04 09:08:12 +0100

    Analyzer etourne le type le plus utilisé par une feature plutot que le type de la première cellule

--- src/main/java/org/opencompare/Analyzer.java
+++ src/main/java/org/opencompare/Analyzer.java
@@ -3,6 +3,7 @@ package org.opencompare;
 import org.opencompare.api.java.*;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -16,15 +17,43 @@ public class Analyzer {
         if (!featureContainer.isEmpty()){
             featureContainer.clear();
         }
-        for (Product product : pcm.getProducts()) {
             for (Feature feature : pcm.getConcreteFeatures()) {
-                if(feature.getCells().get(0).getInterpretation().getClass().getSimpleName().equals("NotAvailableImpl")){
+                HashMap typeContainer = new HashMap<String, Integer>();
+                int i;
+                for (Cell c: feature.getCells()) {
+                    if (!typeContainer.containsKey(c.getInterpretation().getClass().getSimpleName())){
+                        typeContainer.put(c.getInterpretation().getClass().getSimpleName(), 1);
+                    } else {
+                        i = (Integer)typeContainer.get(c.getInterpretation().getClass().getSimpleName());
+                        typeContainer.put(c.getInterpretation().getClass().getSimpleName(), i + 1);
+                    }
+                }
+                System.out.println(typeContainer.toString());
+
+                Iterator it = typeContainer.entrySet().iterator();
+
+                String type = "";
+                int val = 0;
+
+                while (it.hasNext()){
+                    Map.Entry pair = (Map.Entry)it.next();
+                    if (type.equals("")){
+                        type = (String)pair.getKey();
+                        val = (Integer)pair.getValue();
+                    }else{
+                        if ((Integer)pair.getValue() > val){
+                            type = (String)pair.getKey();
+                            val = (Integer)pair.getValue();
+                        }
+                    }
+                }
+
+                if(type.equals("NotAvailableImpl")){
                     featureContainer.put(feature.getName(), "StringValueImpl");
                 }else{
-                    featureContainer.put(feature.getName(), feature.getCells().get(0).getInterpretation().getClass().getSimpleName());
+                    featureContainer.put(feature.getName(), type);
                 }
             }
-        }
     }
 
     public Map getFeatureContainer() {
