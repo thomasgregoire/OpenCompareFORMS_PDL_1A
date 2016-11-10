7335fb68d22b3abbd118d4676d9821c367b9d970
Jules <jules.cohonner@gmail.com>
2016-11-10 15:42:46 +0100

    Ajout de la classe HTMLCreator; Problème avec balise renvoyé dans les types.

---
+++ src/main/java/org/opencompare/HTMLCreator.java
@@ -0,0 +1,99 @@
+package org.opencompare;
+
+import org.opencompare.api.java.PCM;
+import org.opencompare.api.java.PCMContainer;
+import org.opencompare.api.java.impl.io.KMFJSONLoader;
+import org.opencompare.api.java.io.PCMLoader;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Jules on 07/11/2016.
+ */
+
+public class HTMLCreator {
+
+    public String HTMLString(Map<String, String> map) {
+        String texte = "<html>\n" +
+                "    <head>\n" +
+                "        <title>HTML de base</title>\n" +
+                "        <!-- Latest compiled and minified CSS -->\n" +
+                "        <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n" +
+                "        <link rel=\"stylesheet\" href=\"formulaire.css\">\n" +
+                "\n" +
+                "        <script src=\"https://code.jquery.com/jquery-3.1.0.min.js\" defer></script>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "    <h1>HTML crée depuis java</h1>\n" +
+                "\n" +
+                "        <form>\n";
+
+        for (String FeatureBalise : map.keySet()) {
+
+            System.out.println(map.get(FeatureBalise));
+
+            texte = texte + "<div class=\"col-sm-12\">\n" +
+                    "                <label class=\"col-sm-1 col-form-label\">" + FeatureBalise + "</label>\n" +
+                    "                <div class=\"col-sm-6\">\n" +
+                    map.get(FeatureBalise) + "\n" +
+                    "                </div>\n" +
+                    "            </div>\n" +
+                    "            <br /><br />";
+        }
+
+        texte = texte + "            <input type=\"button\" class=\"btn btn-lg btn-primary\" value=\"alert\">\n" +
+                "            <br /><br />\n" +
+                "            <input class=\"btn btn-lg btn-primary\" type=\"reset\" value=\"Reset !\" />\n" +
+                "        </form>" +
+                "    </body>\n" +
+                "\n" +
+                "</html>";
+
+        return texte;
+    }
+
+    public void insertTexte(String text) throws Exception {
+
+        try {
+            BufferedWriter out = new
+                    BufferedWriter(new FileWriter("test.html"));
+            out.write(text);
+            out.close();
+            System.out.println
+                    ("File created successfully");
+        } catch (IOException e) {
+        }
+
+    }
+
+
+    public static void main(String[] Args) throws Exception {
+
+        Analyzer anal = new Analyzer();
+        HTMLGenerator generator = new HTMLGenerator();
+        HTMLCreator creator = new HTMLCreator();
+
+        File pcmFile = new File("pcms/euro.pcm");
+        PCMLoader loader = new KMFJSONLoader();
+        PCM pcm = loader.load(pcmFile).get(0).getPcm();
+
+        anal.findFeatures(pcm);
+
+        creator.insertTexte(creator.HTMLString(generator.GenerateFrom(anal.getFeatureContainer())));
+
+
+    }
+
+}
+
+
+
--- src/main/java/org/opencompare/HTMLGenerator.java
+++ src/main/java/org/opencompare/HTMLGenerator.java
@@ -116,76 +116,76 @@ public class HTMLGenerator
      * @param name
      * @return
      */
-    private static String CreateTextInput(String name) { return "<input type='text' class='balise' name=" + name + ">"; }
+    private static String CreateTextInput(String name) { return "<input type='text' class='form-control' name=" + name + ">"; }
 
     /**
      * Creer un balise de type number specialisé pour les int
      * @param name
      * @return
      */
-    private static String CreateIntegerInput(String name){ return "<input type='number' class='balise' name="+ name +" min='0' step='1'>"; }
+    private static String CreateIntegerInput(String name){ return "<input type='number' class='form-control' name="+ name +" min='0' step='1'>"; }
 
     /**
      * Creerr une balise de type number calibré pour les nombres reels
      * @param name
      * @return
      */
-    private static String CreateRealInput(String name) { return "<input type='number' class='balise' name="+ name + ">"; }
+    private static String CreateRealInput(String name) { return "<input type='form' class='form-control' name="+ name + ">"; }
 
     /**
      * Creer une balise de type number calibree pour des multiples ?
      * @param name
      * @return
      */
-    private static String CreateMultipleInput(String name) { return "<input type='number' class='balise' name="+ name +">"; }
+    private static String CreateMultipleInput(String name) { return "<input type='number' class='form-control' name="+ name +">"; }
 
     /**
      * Creer une balise Partial ?
      * @return
      */
-    private static String CreatePartialInput(String name) { return "<input type='text' class='balise' name="+ name +">"; }
+    private static String CreatePartialInput(String name) { return "<input type='text' class='form-control' name="+ name +">"; }
 
     /**
      * Creer une balise de type texte avec un pattern specifique au versions
      * @return
      */
-    private static String CreateVersionInput(String name) { return "<input type='text' class='balise' name=" + name + " pattern='\\d\\.\\d\\.\\d'>"; }
+    private static String CreateVersionInput(String name) { return "<input type='text' class='form-control' name=" + name + " pattern='\\d\\.\\d\\.\\d'>"; }
 
     /**
      * Creer une balise Unit ?
      * @return
      */
-    private static String CreateUnitInput(String name) { return "<input type='text' class='balise' name="+ name +">"; }
+    private static String CreateUnitInput(String name) { return "<input type='text' class='form-control' name="+ name +">"; }
 
     /**
      * Creer une balise Dimension ?
      * @return
      */
-    private static String CreateDimensionInput(String name) { return "<input type='text' class='balise' name="+ name +">"; }
+    private static String CreateDimensionInput(String name) { return "<input type='text' class='form-control' name="+ name +">"; }
 
     /**
      * Creer une balise Conditionnal ?
      * @return
      */
-    private static String CreateConditionalInput(String name) { return "<input type='text' class='balise' name="+ name +">"; }
+    private static String CreateConditionalInput(String name) { return "<input type='text' class='form-control' name="+ name +">"; }
 
     /**
      * Creer une balise checkbox
      * @return
      */
-    private static String CreateCheckInput(String name) { return "<input type='checkbox' class='balise' name="+ name +" value=''>"+ name +"<br>"; }
+    private static String CreateCheckInput(String name) { return "<input type='checkbox' class='form-control' name="+ name +" value=''>"+ name +"<br>"; }
 
     /**
      * Creer une balise date
      * @return
      */
-    private static String CreateDateInput(String name) { return "<input type='date' class='balise' name="+ name +">"; }
+    private static String CreateDateInput(String name) { return "<input type='date' class='form-control' name="+ name +">"; }
 
     /**
      * Creer une balise erreur
      * @return
      */
-    private static String CreateErrorInput(String name) { return "<input type='text' class='balise' name="+ name +" text='error'>"; }
+    private static String CreateErrorInput(String name) { return "<input type='text' class='form-control' name="+ name +" text='error'>"; }
 
     //endregion
 
