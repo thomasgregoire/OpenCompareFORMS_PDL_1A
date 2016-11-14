ac9f8922b34d0130c0ed1371aa8c7c155c9e93b4
Thomas Gregoire <thomas0035@gmail.com>
2016-11-14 14:41:04 +0100

    Modification retour des méthodes

--- src/main/java/org/opencompare/Analyzer.java
+++ src/main/java/org/opencompare/Analyzer.java
@@ -11,57 +11,58 @@ import java.util.Map;
  */
 public class Analyzer {
 
-    private HashMap featureContainer = new HashMap();
-
-    public void findFeatures(PCM pcm){
-        if (!featureContainer.isEmpty()){
-            featureContainer.clear();
+    public Map getTypeFeatures(PCM pcm) {
+        HashMap featureTypeContainer = new HashMap();
+        if (!featureTypeContainer.isEmpty()) {
+            featureTypeContainer.clear();
         }
-            for (Feature feature : pcm.getConcreteFeatures()) {
-                HashMap typeContainer = new HashMap<String, Integer>();
-                int i;
-                for (Cell c: feature.getCells()) {
-                    if (!typeContainer.containsKey(c.getInterpretation().getClass().getSimpleName())){
-                        typeContainer.put(c.getInterpretation().getClass().getSimpleName(), 1);
-                    } else {
-                        i = (Integer)typeContainer.get(c.getInterpretation().getClass().getSimpleName());
-                        typeContainer.put(c.getInterpretation().getClass().getSimpleName(), i + 1);
-                    }
+        for (Feature feature : pcm.getConcreteFeatures()) {
+            HashMap typeContainer = new HashMap<String, Integer>();
+            int i;
+            for (Cell c : feature.getCells()) {
+                if (!typeContainer.containsKey(c.getInterpretation().getClass().getSimpleName())) {
+                    typeContainer.put(c.getInterpretation().getClass().getSimpleName(), 1);
+                } else {
+                    i = (Integer) typeContainer.get(c.getInterpretation().getClass().getSimpleName());
+                    typeContainer.put(c.getInterpretation().getClass().getSimpleName(), i + 1);
                 }
-                System.out.println(typeContainer.toString());
+            }
+            System.out.println(typeContainer.toString());
 
-                Iterator it = typeContainer.entrySet().iterator();
+            Iterator it = typeContainer.entrySet().iterator();
 
-                String type = "";
-                int val = 0;
+            String type = "";
+            int val = 0;
 
-                while (it.hasNext()){
-                    Map.Entry pair = (Map.Entry)it.next();
-                    String key = (String)pair.getKey();
-                    int value = (Integer)pair.getValue();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                String key = (String) pair.getKey();
+                int value = (Integer) pair.getValue();
 
-                    if (!key.equals("NotAvailableImpl") && !key.equals("NotApplicableImpl") && !key.equals("ValueImpl")){
-                        if (type.equals("")){
+                if (!key.equals("NotAvailableImpl") && !key.equals("NotApplicableImpl") && !key.equals("ValueImpl")) {
+                    if (type.equals("")) {
+                        type = key;
+                        val = value;
+                    } else {
+                        if ((Integer) pair.getValue() > val) {
                             type = key;
                             val = value;
-                        }else{
-                            if ((Integer)pair.getValue() > val){
-                                type = key;
-                                val = value;
-                            }
                         }
                     }
                 }
-
-                if(type.equals("")){
-                    featureContainer.put(feature.getName(), "StringValueImpl");
-                }else{
-                    featureContainer.put(feature.getName(), type);
-                }
             }
+
+            if (type.equals("")) {
+                featureTypeContainer.put(feature.getName(), "StringValueImpl");
+            } else {
+                featureTypeContainer.put(feature.getName(), type);
+            }
+        }
+        return featureTypeContainer;
     }
 
-    public Map getFeatureContainer() {
-        return featureContainer;
+    public Map getContentFeatures(PCM pcm) {
+        HashMap featureContentContainer = new HashMap();
+        return featureContentContainer;
     }
 }
