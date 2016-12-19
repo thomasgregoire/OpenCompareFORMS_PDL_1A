7c54301073642178b1569fd3b358a131a9b38813
Thomas Gregoire <thomas0035@gmail.com>
2016-12-19 18:19:06 +0100

    Ajout de commentaires pour la classe Analyzer

--- src/main/java/org/opencompare/Analyzer.java
+++ src/main/java/org/opencompare/Analyzer.java
@@ -15,7 +15,13 @@ public class Analyzer {
     private final int POURCENTAGE = 7;
     private final int POURCENTAGE_CONTENT = 10;
 
-
+    /***
+     *
+     * @param em
+     * @param pcm
+     * @return HashMap <String, ArrayList<String>>
+     *  with the name of the features of pcm as key and a list of it's associated types as value
+     */
     public Map getTypeFeatures(ExportMatrix em, PCM pcm) {
         HashMap featureTypeContainer = new HashMap<String, ArrayList<String>>();
         Collection<String> nomFeature = getNomFeature(em);
@@ -25,7 +31,7 @@ public class Analyzer {
             featureTypeContainer.clear();
         }
         for (Feature feature : pcm.getConcreteFeatures()) {
-            if (nomFeature.contains(feature.getName())) {
+            if (nomFeature.contains(feature.getName())) { //seul le plus bas niveau de chaque features est traité
                 HashMap typeContainer = new HashMap<String, Integer>();
                 List lesTypes = new ArrayList<String>();
                 int i;
@@ -39,6 +45,7 @@ public class Analyzer {
                         }
                     }
                 }
+                // les types non utiles sont ignorés
                 typeContainer.remove("NotApplicableImpl");
                 typeContainer.remove("NotAvailableImpl");
                 typeContainer.remove("ValueImpl");
@@ -68,14 +75,14 @@ public class Analyzer {
                             val2 = value;
                         }
                     }
-
+                    //Le type qui apparaît le plus est ajouté à la liste
                     lesTypes.add(type);
 
+                    //Le deuxième type est ajouté si son purcentage d'apparition est assez élévé
                     double pourc = val2 / (typeContainer.size()) * 100;
                     if (pourc > POURCENTAGE) {
                         lesTypes.add(type2);
                     }
-                    System.out.println(lesTypes.toString());
                     featureTypeContainer.put(feature.getName(), lesTypes);
                 } else {
                     lesTypes.add("StringValueImpl");
@@ -86,6 +93,15 @@ public class Analyzer {
         return featureTypeContainer;
     }
 
+    /***
+     *
+     * @param em
+     * @param pcm
+     * @return HashMap <String, ArrayList<String>
+     *     with the names of the features of pcm as key and a list of the associated cell contents as value.
+     *
+     *  This method will be used for the auto-completion of the generated form.
+     */
     public Map getContentFeatures(ExportMatrix em, PCM pcm) {
         HashMap featureContentContainer = new HashMap<String, ArrayList<String>>();
 
@@ -94,7 +110,7 @@ public class Analyzer {
             featureContentContainer.clear();
         }
        for(Feature feature : pcm.getConcreteFeatures()){
-           if(nomFeature.contains(feature.getName())){
+           if(nomFeature.contains(feature.getName())){ //seul le plus bas niveau de chaque features est traité
                HashMap contentContainer = new HashMap<String, Integer>();
                List listContent = new ArrayList<String>();
                int i;
@@ -123,11 +139,14 @@ public class Analyzer {
                featureContentContainer.put(feature.getName(),listContent);
            }
        }
-        System.out.println(featureContentContainer);
         return featureContentContainer;
     }
 
-
+    /***
+     *
+     * @param em
+     * @return an ArrayList<String> of the names of the lowest level of the different features of em
+     */
     private Collection<String> getNomFeature(ExportMatrix em){
         Collection<String> nomFeature = new ArrayList<String>();
         String nom = "";
